5563dff2702c3daa756e9419df04c74bd6ad3cd9
Shane Alcock <salcock@waikato.ac.nz>
2019-03-08 16:45:36 +1300

    Update bintray-upload.sh to Brad's latest version

--- bintray-upload.sh
+++ bintray-upload.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-set -x -e -o pipefail
+set -e -o pipefail
+
+BINTRAY_REPO="wand/openli"
+BINTRAY_LICENSE="GPL-3.0"
 
 apt-get update && apt-get install -y curl
 
-for path in `find built-packages/amd64/ -maxdepth 1 -type d`; do
-    IFS=_ read linux_dist linux_version <<< $(basename "${path}")
+curl --silent -fL -XGET \
+    "https://api.bintray.com/content/jfrog/jfrog-cli-go/\$latest/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64" \
+    > /usr/local/bin/jfrog
+chmod +x /usr/local/bin/jfrog
+mkdir ~/.jfrog/
+cat << EOF > ~/.jfrog/jfrog-cli.conf
+{
+  "artifactory": null,
+  "bintray": {
+    "user": "${BINTRAY_USERNAME}",
+    "key": "${BINTRAY_API_KEY}"
+  },
+  "Version": "1"
+}
+EOF
+
+for path in `find built-packages/ -maxdepth 1 -type d`; do
+    IFS=_ read linux_version <<< $(basename "${path}")
     for deb in `find "${path}" -maxdepth 1 -type f`; do
         pkg_filename=$(basename "${deb}")
         IFS=_ read pkg_name pkg_version pkg_arch <<< $(basename -s ".deb" "${pkg_filename}")
-        curl -T "${deb}" -u${BINTRAY_USERNAME}:${BINTRAY_API_KEY} \
-            "https://api.bintray.com/content/wand/OpenLI/${pkg_name}/${pkg_version}/pool/${linux_version}/main/${pkg_filename};deb_distribution=${linux_version};deb_component=main;deb_architecture=${pkg_arch}"
+        jfrog bt package-create --licenses ${BINTRAY_LICENSE} --vcs-url ${CI_PROJECT_URL} ${BINTRAY_REPO}/${pkg_name} || true
+        jfrog bt upload --publish --deb ${linux_version}/main/${pkg_arch} ${deb} ${BINTRAY_REPO}/${pkg_name}/${pkg_version} pool/${linux_version}/main/${pkg_name}/
     done
 done
+
