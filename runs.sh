2df79d8b0fb0f1eb426c1556c3bb30bb1a33b74a
Tyler Marriner <marriner.tyler@gmail.com>
2019-05-28 14:29:43 +1200

    tidyed up changes

--- runs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#./src/openliprovisioner -c doc/exampleconfigs/provisioner-example.yaml
-
-#./src/openlimediator -c doc/exampleconfigs/mediator2-example.yaml
-
-#./src/openlimediator -c doc/exampleconfigs/mediator-example.yaml
