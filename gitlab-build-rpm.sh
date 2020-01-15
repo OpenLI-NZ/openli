d20a95be53fc1560583baaa650c4f8e09440279e
Shane Alcock <salcock@waikato.ac.nz>
2020-01-15 16:15:11 +1300

    Force centos8 build to use our Judy packages
    If the mariadb module is enabled, yum will prefer the
    1.0.5-18 Judy package in the AppStream repo. This is not
    compatible with our 1.0.5-22 Judy-devel package (for some
    inexplicable reason), so we need to force mariadb to be disabled
    to ensure our Judy 1.0.5-22 package is preferred for building.

--- gitlab-build-rpm.sh
+++ gitlab-build-rpm.sh
@@ -60,6 +60,7 @@ if [ "$1" = "centos8" ]; then
         yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm || true
         dnf install -y 'dnf-command(config-manager)' || true
         yum config-manager --set-enabled PowerTools || true
+        yum module -y disable mariadb
 fi
 
 
