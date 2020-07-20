c1de5dae7a8d00ad4ee8f9466b8ef32a29e5aee9
Shane Alcock <salcock@waikato.ac.nz>
2020-07-21 10:49:05 +1200

    Use cloudsmith repos for packaging dependencies

--- debpkg-setup.sh
+++ debpkg-setup.sh
@@ -18,6 +18,7 @@ DISTRO=$(lsb_release -sc)
 curl -1sLf 'https://dl.cloudsmith.io/public/wand/libwandio/cfg/setup/bash.deb.sh' | bash
 curl -1sLf 'https://dl.cloudsmith.io/public/wand/libwandder/cfg/setup/bash.deb.sh' | bash
 curl -1sLf 'https://dl.cloudsmith.io/public/wand/libtrace/cfg/setup/bash.deb.sh' | bash
+curl -1sLf 'https://dl.cloudsmith.io/public/wand/openli/cfg/setup/bash.deb.sh' | bash
 
 case ${DISTRO} in
         jessie | xenial | stretch )
