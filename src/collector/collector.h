92bf1553a8aef921dda8bb41bd8941632286eb33
Shane Alcock <shane@alcock.co.nz>
2023-11-22 18:11:23 +1300

    Remove (now) unused 'trust_sip_from' members

--- src/collector/collector.h
+++ src/collector/collector.h
@@ -327,7 +327,6 @@ typedef struct collector_global {
     pthread_mutex_t stats_mutex;
 
     uint8_t etsitls;
-    uint8_t trust_sip_from;
 
     uint8_t encoding_method;
     openli_ssl_config_t sslconf;
