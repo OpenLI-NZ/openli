9cf03c6b8b3c8e2ab2aa270dd42ab76a01f5dc73
Shane Alcock <shane@alcock.co.nz>
2024-05-31 05:58:01 +0000

    forwarder: fix retransmits due to RMQ consumer timeout
    The heartbeat message was being sent to RMQ clients over the
    regular TCP socket, so it was not having the intended effect of
    forcing the mediator to periodically acknowledge deliveries.
    
    This was also the underlying cause of unexpected retransmits
    of ETSI records that had already been received by the LEA.

--- src/export_buffer.h
+++ src/export_buffer.h
@@ -77,6 +77,7 @@ void release_export_buffer(export_buffer_t *buf);
 uint64_t get_buffered_amount(export_buffer_t *buf);
 uint64_t append_message_to_buffer(export_buffer_t *buf,
         openli_encoded_result_t *msg, uint32_t beensent);
+uint64_t append_heartbeat_to_buffer(export_buffer_t *buf);
 uint64_t append_etsipdu_to_buffer(export_buffer_t *buf,
         uint8_t *pdustart, uint32_t pdulen, uint32_t beensent);
 int transmit_buffered_records(export_buffer_t *buf, int fd,
