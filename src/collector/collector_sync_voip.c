OpenLI-NZ/openli

commit f3fe7624c1d310cb60e6e39ea63aca3975a63755

Fix RTP intercept failures if SIP is proxied back to the source IP
Bug was introduced by
https://github.com/OpenLI-NZ/openli/commit/0049127c86ec813252f02778a12e074f751dfc14

In certain setups, a SIP proxy may end up proxying a SIP session
back to the exact same IP address that sent the initial SIP
messages in the first place. In other words, we see an
INVITE from IP A -> proxy B, followed by the same (mostly)
INVITE going back from proxy B to IP A.

In that case, we were erroneously determining that the proxied
INVITE was a "reverse INVITE" and acting as though the SDP info
in that INVITE applied to the other direction of the corresponding
RTP stream. If the proxy had forwarded that INVITE anywhere else
then there wouldn't be a problem, but the destination IP matching
the source of the initial INVITE would trigger the "reverse INVITE"
logic.

This change simply forces any INVITE seen prior to the RTP stream
becoming "active", i.e. the 200 OK being seen for the most recent
INVITE, to be treated as an initial INVITE regardless of its
destination IP address.

Note to self: I guess there is still potential for things to go
haywire if a proxy is sending back to the same source IP AND a
genuine reverse INVITE occurs....

src/collector/collector_sync_voip.c
@@ -1271,7 +1271,7 @@ static int process_sip_invite(collector_sync_voip_t *sync, char *callid,
         portstr = get_sip_media_port(sync->sipparser, 0);
         mediatype = get_sip_media_type(sync->sipparser, 0);
 
-        if (iritype == ETSILI_IRI_BEGIN) {
+        if (iritype == ETSILI_IRI_BEGIN || thisrtp->active == 0) {
             memcpy(thisrtp->inviter, irimsg->data.ipmmiri.ipsrc, 16);
             dir = 0;
         } else if (memcmp(thisrtp->inviter, irimsg->data.ipmmiri.ipsrc,

commit c2957d9777e0abec0f61c52a187928386a211379

Fix RTP intercept failures for reverse INVITES (again)
Fixes bug introduced in f3fe7624c1d310cb60e6e39ea63aca3975a63755
where a proxied SIP session would set the "inviter" incorrectly.

This meant that a reverse INVITE would not be correctly handled,
and therefore RTP traffic could be missed.

src/collector/collector_sync_voip.c
@@ -787,6 +787,7 @@ static int process_sip_183sessprog(collector_sync_voip_t *sync,
             }
         }
 
+
         announce_rtp_streams_if_required(sync, thisrtp);
     }
     free(cseqstr);
@@ -1168,6 +1169,7 @@ static int process_sip_invite(collector_sync_voip_t *sync, char *callid,
     openli_sip_identity_set_t all_identities;
     uint8_t trust_sip_from;
     openli_location_t *locptr;
+    char *invitecseq = NULL;
 
     locptr = NULL;
     loc_cnt = 0;
@@ -1192,6 +1194,7 @@ static int process_sip_invite(collector_sync_voip_t *sync, char *callid,
     trust_sip_from = sync->info->trust_sip_from;
     pthread_rwlock_unlock(sync->info_mutex);
 
+    invitecseq = get_sip_cseq(sync->sipparser);
     HASH_ITER(hh_liid, sync->voipintercepts, vint, tmp) {
         vshared = NULL;
 
@@ -1271,7 +1274,9 @@ static int process_sip_invite(collector_sync_voip_t *sync, char *callid,
         portstr = get_sip_media_port(sync->sipparser, 0);
         mediatype = get_sip_media_type(sync->sipparser, 0);
 
-        if (iritype == ETSILI_IRI_BEGIN || thisrtp->active == 0) {
+        if (iritype == ETSILI_IRI_BEGIN || (
+                thisrtp->invitecseq && invitecseq &&
+                strcmp(thisrtp->invitecseq, invitecseq) == 0)) {
             memcpy(thisrtp->inviter, irimsg->data.ipmmiri.ipsrc, 16);
             dir = 0;
         } else if (memcmp(thisrtp->inviter, irimsg->data.ipmmiri.ipsrc,
@@ -1312,13 +1317,17 @@ static int process_sip_invite(collector_sync_voip_t *sync, char *callid,
             continue;
         }
 
-        thisrtp->invitecseq = get_sip_cseq(sync->sipparser);
+        thisrtp->invitecseq = invitecseq;
+        invitecseq = NULL;
 
         create_sip_ipiri(sync, vint, irimsg, iritype, vshared->cin, locptr,
                 loc_cnt, pkts, pkt_cnt);
         exportcount += 1;
     }
 
+    if (invitecseq) {
+        free(invitecseq);
+    }
     if (locptr) {
         free(locptr);
     }