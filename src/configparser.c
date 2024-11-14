49c92787ec428f4be553fdcb1f453e17b871d360
Shane Alcock <shane@alcock.co.nz>
2024-11-14 17:36:55 +1300

    Allow 'smsthreads' config option to configure number of SIP threads
    This means that old configs that set smsthreads will have the same
    number of threads available for processing SMS over SIP, but those
    threads will also be available for processing SIP calls.

--- src/configparser.c
+++ src/configparser.c
@@ -1317,6 +1317,8 @@ static int global_parser(void *arg, yaml_document_t *doc,
             key, value, "email worker", 0);
     parse_col_thread_count(&(glob->gtp_threads), "gtpthreads",
             key, value, "GTP worker", 0);
+    parse_col_thread_count(&(glob->sip_threads), "smsthreads",
+            key, value, "SIP worker", 0);
     parse_col_thread_count(&(glob->sip_threads), "sipthreads",
             key, value, "SIP worker", 0);
 
