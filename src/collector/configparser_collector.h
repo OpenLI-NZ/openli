0d5bce9ddb09fbb8f8ffdb228ae268f6d9b226fb
Shane Alcock <shane@alcock.co.nz>
2025-01-21 17:36:15 +1300

    Separate config parsing code into 1 file per component
    This fixes an issue where the provisioner and mediator
    needed to include collector-specific code in their builds
    just because it was required by the monolith configparser.c.
    
    It also has the added benefit of making the config parsing
    code a bit less of a mess.

---
+++ src/collector/configparser_collector.h
@@ -0,0 +1,35 @@
+/*
+ *
+ * Copyright (c) 2024 SearchLight Ltd, New Zealand.
+ * All rights reserved.
+ *
+ * This file is part of OpenLI.
+ *
+ * This code has been developed by the University of Waikato WAND
+ * research group. For further information please see http://www.wand.net.nz/
+ *
+ * OpenLI is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenLI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+#ifndef OPENLI_CONFIGPARSER_COLLECTOR_H_
+#define OPENLI_CONFIGPARSER_COLLECTOR_H_
+
+#include "configparser_common.h"
+#include "collector/collector.h"
+#include "collector/x2x3_ingest.h"
+
+int parse_collector_config(char *configfile, collector_global_t *glob);
+#endif
