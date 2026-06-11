OpenLI-NZ/openli

commit 2c6afcda6dddb00e213c185de337c9c869381dc4

SIP: fix bug with updating RTP properties for reverse INVITEs

src/collector/sip_update_state.c
@@ -910,7 +910,7 @@ static int process_sip_response(openli_sip_worker_t *sipworker,
         if (mediatype == NULL) {
             goto responseover;
         }
-        if (dir == ETSI_DIR_TO_TARGET) {
+        if (dir == ETSI_DIR_TO_TARGET || dir == ETSI_DIR_FROM_TARGET) {
             r = extract_media_streams_from_sdp(thisrtp, sipworker->sipparser,
                         dir);
             if (r < 0) {

commit 0cdd6d6e6f03c549ca42429da2f1a610adcefa68

Fix bug that broke interception for calls using a SIP proxy loop
2c6afcd introduced this bug, which is resolved by separating
"call" direction from the current INVITE's direction.

src/collector/sip_update_state.c
@@ -418,6 +418,7 @@ static uint8_t apply_invite_cseq_to_call(rtpstreaminf_t *thisrtp,
         if (dir != 0xff && thisrtp->invitecseq == NULL) {
             // this is the first INVITE for the call
             thisrtp->invitecseq = strdup(invitecseq);
+            thisrtp->invitecseq_dir = dir;
         } else if (thisrtp->invitecseq != NULL &&
                 strcmp(invitecseq, thisrtp->invitecseq) == 0) {
             // this is a copy of the original INVITE
@@ -426,6 +427,7 @@ static uint8_t apply_invite_cseq_to_call(rtpstreaminf_t *thisrtp,
             free(thisrtp->invitecseq);
             thisrtp->invitecseq = NULL;
             thisrtp->invitecseq = strdup(invitecseq);
+            thisrtp->invitecseq_dir = dir;
         }
     }
 
@@ -957,7 +959,8 @@ static int process_sip_response(openli_sip_worker_t *sipworker,
         if (mediatype == NULL) {
             goto responseover;
         }
-        if (dir == ETSI_DIR_TO_TARGET || dir == ETSI_DIR_FROM_TARGET) {
+        if ((dir == ETSI_DIR_TO_TARGET || dir == ETSI_DIR_FROM_TARGET)
+                && dir != thisrtp->invitecseq_dir) {
             r = extract_media_streams_from_sdp(thisrtp, sipworker->sipparser,
                         dir);
             if (r < 0) {

src/intercept.h
@@ -544,6 +544,7 @@ struct rtpstreaminf {
     uint8_t changed;
     uint8_t byematched;
     char *invitecseq;
+    uint8_t invitecseq_dir;
     char *byecseq;
 
     uint8_t inviter[16];